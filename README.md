b4733d2cebba929e2fd63060b0b69cad64ddc789
Franklin Chen <franklinchen@franklinchen.com>
2015-09-21 22:39:06 -0400

    Initial.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Get data from URL, in Haskell
+
+[![Build Status](https://travis-ci.org/FranklinChen/get-data-from-url-haskell.png)](https://travis-ci.org/FranklinChen/get-data-from-url-haskell)
+
+Haskell version of a [Rust program](https://github.com/hjr3/rust-get-data-from-url) described in a [blog post](http://hermanradtke.com/2015/09/21/get-data-from-a-url-rust.html).
+
+## Instructions
+
+```console
+$ stack build
+```
+
+```console
+$ stack exec get-data-from-url
+```
+
+Or, to install the program as `~/.local/bin/get-data-from-url`:
+
+```console
+$ stack install
+```
---
+++ src/Main.hs
@@ -0,0 +1,31 @@
+-- | Haskell version of a <http://hermanradtke.com/2015/09/21/get-data-from-a-url-rust.html Rust program>.
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Network.Wreq (get, responseStatus, responseBody, statusCode)
+import Control.Monad (when)
+import Control.Lens ((^.), (^?), itraverseOf_)
+import Data.Aeson.Lens (key, members, _String, AsValue)
+import qualified Data.Text as Text
+
+url :: String
+url = "https://www.hautelook.com/api"
+
+main :: IO ()
+main = do
+  r <- get url
+  let code = r ^. responseStatus . statusCode
+  when (code /= 200) $
+    fail ("Unable to handle HTTP response code " ++ show code)
+  case r ^? responseBody . key "_links" of
+    Nothing -> fail "Failed to get '_links' value from json"
+    Just links -> itraverseOf_ members printLink links
+
+printLink :: AsValue s => Text.Text -> s -> IO ()
+printLink rel link =
+  case link ^? key "href" . _String of
+    Nothing -> fail ("Failed to get 'href' value from '_links' at key " ++
+                     Text.unpack rel)
+    Just href -> putStrLn (Text.unpack rel ++ " -> " ++ Text.unpack href)
